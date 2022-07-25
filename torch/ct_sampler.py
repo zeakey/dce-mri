9ee598f50c2440c68cdd25922bb5d17f8b878a1f
Kai ZHAO <694691@qq.com>
2022-07-24 22:18:27 -0700

    sampler

--- torch/ct_sampler.py
+++ torch/ct_sampler.py
@@ -6,12 +6,12 @@ from torch.distributions import Beta, Uniform
 class CTSampler(object):
     def __init__(self):
         super().__init__()
-        self.ktrans_sampler = Beta(concentration1=1.3, concentration0=13)
-        self.kep_sampler = Beta(concentration1=1.3, concentration0=20)
+        self.ktrans_sampler = Beta(concentration1=2, concentration0=5)
+        self.kep_sampler = Beta(concentration1=2, concentration0=5)
         self.t0_sampler = Uniform(low=0, high=0.5)
 
     def sample(self, n):
-        ktrans = self.ktrans_sampler.sample((n, 1))
-        kep = self.kep_sampler.sample((n, 1))
+        ktrans = self.ktrans_sampler.sample((n, 1)) / 2
+        kep = self.kep_sampler.sample((n, 1)) * 2
         t0 = self.t0_sampler.sample((n, 1))
         return torch.cat((ktrans, kep, t0), dim=1)
