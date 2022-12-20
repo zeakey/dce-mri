1b3fa889b1f729fd3da10ff7368c1a208edfaf3f
Kai ZHAO <694691@qq.com>
2022-12-19 18:23:40 -0800

    cfg

--- torch/configs/noise_scale.py
+++ torch/configs/noise_scale.py
@@ -1,5 +1,7 @@
 _base_ = ['./config.py']
 
-model = dict(num_outputs=4)
+model = dict(
+    num_outputs=4,
+    output_keys=['ktrans', 'kep', 't0', 'noise_scale'])
 
 custom_imports = dict(imports=['models'], allow_failed_imports=False)
