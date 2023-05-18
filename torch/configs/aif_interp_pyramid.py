8af71fecbf273b219b29d66249838ad1e49591b7
Kai ZHAO <694691@qq.com>
2023-05-17 17:35:09 -0700

    aif_interp_pyramid

---
+++ torch/configs/aif_interp_pyramid.py
@@ -0,0 +1,19 @@
+_base_ = ['config.py']
+
+aif = 'mixed'
+
+model = dict(
+    num_outputs=5,
+    pyramid_sigmas=(1,2,4,6),
+    num_fourier_features=4,
+    output_keys=['ktrans', 'kep', 't0', 'beta', 'noise_scale'])
+
+sampler = dict(
+    beta_sampler = dict(
+        type='Uniform',
+        low=0,
+        high=1,
+    ),
+)
+
+custom_imports = dict(imports=['models'], allow_failed_imports=False)
